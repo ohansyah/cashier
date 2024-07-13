184674921dba3abcf87edccd96a18a63d16b2730
ohansyah <ohan.ohansyah@gmail.com>
2024-07-13 11:53:39 +0700

    adjust: resize products card

--- resources/views/livewire/cashier.blade.php
+++ resources/views/livewire/cashier.blade.php
@@ -1,9 +1,12 @@
-<div class="w-full mx-auto p-4">
-    <div class="flex space-x-4">
+<div class="py-6">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
         <div class="w-full">
 
+            {{-- Filters --}}
             <div class="flex items-center space-x-4 py-4 overflow-x-auto no-scrollbar">
+
+                {{-- Search --}}
                 <div class="flex-grow lg:flex-grow-0 lg:w-1/3">
                     <div class="relative">
                         <input wire:model.live='searchQuery' type="search" id="search" placeholder="Search..."
@@ -22,7 +25,7 @@
                         wire:loading.attr="disabled"
                         wire:target="toggleCategory({{ $category['id'] }})"
                         @class([
-                            'flex-shrink-0 flex items-center rounded-lg p-2 transition duration-200 ease-in-out cursor-pointer border',
+                            'flex-shrink-0 flex items-center rounded-lg p-2 transition duration-200 ease-in-out cursor-pointer border bg-white',
                             'border-indigo-500' => in_array($category['id'], $selectedCategories),
                             'hover:border-indigo-500 focus:outline-none' => !in_array($category['id'], $selectedCategories),
                         ])>
@@ -41,35 +44,38 @@
             </div>
 
             <!-- Products -->
-            <div>
-                <div class="grid grid-cols-6 gap-2">
+            <div class="container">
+                <div class="flex flex-wrap">
                     @foreach ($products as $product)
-                    <button
-                        wire:click="addToCart({{ $product['id'] }})"
-                        wire:loading.attr="disabled"
-                        wire:target="addToCart({{ $product['id'] }})"
-                        @class([
-                            'border p-4 rounded-lg bg-white focus:outline-none cursor-pointer transition duration-200 ease-out',
-                            'border-indigo-500' => in_array($product['id'], $cartItemsProductIds),
-                            'hover:border-indigo-500 focus:outline-none' => !in_array($product['id'], $cartItemsProductIds),
-                        ])>
-
-                        <div>
-                            <!-- Loading Animation -->
-                            <div wire:loading wire:target="addToCart({{ $product['id'] }})">
-                                <div class="w-full h-32 flex justify-center items-center">
-                                    @svg('css-spinner', 'w-16 h-16 animate-spin text-indigo-500')
+                        <div class="flex grow justify-center items-center mb-2">
+                            <button
+                                wire:click="addToCart({{ $product['id'] }})"
+                                wire:loading.attr="disabled"
+                                wire:target="addToCart({{ $product['id'] }})"
+                                @class([
+                                    'border p-4 rounded-lg bg-white focus:outline-none cursor-pointer transition duration-200 ease-out shadow-md overflow-hidden',
+                                    'border-indigo-500' => in_array($product['id'], $cartItemsProductIds),
+                                    'hover:border-indigo-500 focus:outline-none' => !in_array($product['id'], $cartItemsProductIds),
+                                ])>
+        
+                                <div>
+                                    <div class="w-36 h-32 flex justify-center items-center">
+                                        <!-- Loading Animation -->
+                                        <div wire:loading wire:target="addToCart({{ $product['id'] }})">
+                                            @svg('css-spinner', 'w-16 h-16 object-cover animate-spin text-indigo-500')
+                                        </div>
+                                        <div wire:loading.remove wire:target="addToCart({{ $product['id'] }})">
+                                            <img src="{{ $product->image_url }}" alt="{{ $product['name'] }}" class="object-cover">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="mt-2">
+                                        <h3 class="font-semibold">{{ $product['name'] }}</h3>
+                                        <p class="text-indigo-500">{{ $product->price_formatted }}</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div wire:loading.remove wire:target="addToCart({{ $product['id'] }})">
-                                <img src="{{ $product->image_url }}" alt="{{ $product['name'] }}" class="w-full h-32 object-cover">
-                            </div>
-                            <div class="mt-2">
-                                <h3 class="font-semibold">{{ $product['name'] }}</h3>
-                                <p class="text-indigo-500">{{ $product->price_formatted }}</p>
-                            </div>
+                            </button>
                         </div>
-                    </button>
                     @endforeach
                 </div>
 
@@ -78,13 +84,13 @@
                 </div>
 
             </div>
-
-
+            
         </div>
         
         @if (count($cartItems) > 0)
-            <div class="fixed bottom-0 left-0 right-0 px-4 py-2 bg-white shadow-lg">
-                <div class="flex flex-row space-x-4">
+        <div class="fixed bottom-0 left-0 right-0 bg-white shadow-lg">
+            <div class="max-w-7xl mx-auto ">
+                <div class="flex flex-row space-x-4 px-4 py-2">
                     
                     <button wire:click="clearCart" class="basis-1/3 flex items-center justify-center text-red-500 hover:text-white hover:bg-red-500 p-2 rounded transition-all ease-in-out duration-250 border border-red-500 w-full">
                         <div class="flex items-center space-x-2">
@@ -116,6 +122,7 @@
 
                 </div>
             </div>
+        </div>
         @endif
 
     </div>
