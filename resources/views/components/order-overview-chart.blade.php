cb938230077c139b091114060ee80c79108763b3
ohansyah <ohan.ohansyah@gmail.com>
2024-08-17 22:45:44 +0700

    feat: slicing dashboard - order overview card - order overview chart

---
+++ resources/views/components/order-overview-chart.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-wrap mb-2">
+    <div class="flex-auto bg-white overflow-hidden shadow-md rounded-lg text-left p-6 m-1 lg:mr-2">
+    <p class="text-gray-600 text-sm">Visualize Order Overview Chart</p>
+    </div>
+</div>
