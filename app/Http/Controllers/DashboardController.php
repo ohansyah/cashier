8eaa1d7e653da9c215745ffa1a5007b641274039
ohansyah <ohan.ohansyah@gmail.com>
2024-08-18 00:05:34 +0700

    format: auto beautify

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Charts\OrderOverviewChart;
 use App\Models\Order;
 
@@ -17,17 +16,17 @@ class DashboardController extends Controller
         $sumOrderTotalYesterday = (int) Order::whereDate('created_at', now()->subDay())->sum('total');
 
         // percentage orders.total today vs orders.total yesterday
-        $percentageOrderTotal = $sumOrderTotalYesterday ? (($sumOrderTotalToday - $sumOrderTotalYesterday) / $sumOrderTotalYesterday) * 100 : 100;
+        $percentageOrderTotal = $sumOrderTotalYesterday ? (($sumOrderTotalToday - $sumOrderTotalYesterday) / $sumOrderTotalYesterday) * 100 : 0;
         $percentageOrderTotal = round($percentageOrderTotal);
-        
+
         // count orders today
         $countOrderToday = (int) Order::whereDate('created_at', now())->count();
 
         // count orders yesterday
         $countOrderYesterday = (int) Order::whereDate('created_at', now()->subDay())->count();
-        
+
         // percentage orders today vs orders yesterday
-        $percentageOrder = $countOrderYesterday ? (($countOrderToday - $countOrderYesterday) / $countOrderYesterday) * 100 : 100;
+        $percentageOrder = $countOrderYesterday ? (($countOrderToday - $countOrderYesterday) / $countOrderYesterday) * 100 : 0;
         $percentageOrder = round($percentageOrder);
 
         // format currency
@@ -35,7 +34,7 @@ class DashboardController extends Controller
 
         return view('dashboard', [
             'card' => compact('sumOrderTotalToday', 'percentageOrderTotal', 'countOrderToday', 'percentageOrder'),
-            'chart' => $chart->build()
+            'chart' => $chart->build(),
         ]);
     }
 }
